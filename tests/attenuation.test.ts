59b1d00fe974772efe1f6832c75f153dc88ac75f
Philipp Krüger <philipp.krueger1@gmail.com>
2021-12-09 18:01:31 +0100

    Write failing test

--- tests/attenuation.test.ts
+++ tests/attenuation.test.ts
@@ -6,7 +6,7 @@ import { alice, bob, mallory } from "./fixtures"
 
 describe("attenuation.emailCapabilities", () => {
 
-    it("works with an example", async () => {
+    it("works with a simple example", async () => {
         // alice -> bob, bob -> mallory
         // alice delegates access to sending email as her to bob
         // and bob delegates it further to mallory
@@ -38,7 +38,7 @@ describe("attenuation.emailCapabilities", () => {
         }])
     })
 
-    it("will report the first issuer in the chain as originator", async () => {
+    it("reports the first issuer in the chain as originator", async () => {
         // alice -> bob, bob -> mallory
         // alice delegates nothing to bob
         // and bob delegates his email to mallory
@@ -66,7 +66,7 @@ describe("attenuation.emailCapabilities", () => {
         }])
     })
 
-    it("will find the right proof chain for the originator", async () => {
+    it("finds the right proof chain for the originator", async () => {
         // alice -> mallory, bob -> mallory, mallory -> alice
         // both alice and bob delegate their email access to mallory
         // mallory then creates a UCAN with capability to send both
@@ -117,6 +117,54 @@ describe("attenuation.emailCapabilities", () => {
                 originator: bob.did(),
                 expiresAt: Math.min(leafUcanBob.payload.exp, ucan.payload.exp),
                 email: "bob@email.com",
+                potency: "SEND",
+            }
+        ])
+    })
+
+    it("reports all chain options", async () => {
+        // alice -> mallory, bob -> mallory, mallory -> alice
+        // both alice and bob claim to have access to alice@email.com
+        // and both grant that capability to mallory
+        // a verifier needs to know both to verify valid email access
+
+        const aliceEmail = {
+            email: "alice@email.com",
+            cap: "SEND",
+        }
+
+        const leafUcanAlice = await token.build({
+            issuer: alice,
+            audience: mallory.did(),
+            capabilities: [aliceEmail]
+        })
+
+        const leafUcanBob = await token.build({
+            issuer: bob,
+            audience: mallory.did(),
+            capabilities: [aliceEmail]
+        })
+
+        const ucan = await token.build({
+            issuer: mallory,
+            audience: alice.did(),
+            capabilities: [aliceEmail],
+            proofs: [token.encode(leafUcanAlice), token.encode(leafUcanBob)]
+        })
+
+        const chained = await Chained.fromToken(token.encode(ucan))
+
+        expect(emailCapabilities(chained)).toEqual([
+            {
+                originator: alice.did(),
+                expiresAt: Math.min(leafUcanAlice.payload.exp, ucan.payload.exp),
+                email: "alice@email.com",
+                potency: "SEND",
+            },
+            {
+                originator: bob.did(),
+                expiresAt: Math.min(leafUcanBob.payload.exp, ucan.payload.exp),
+                email: "alice@email.com",
                 potency: "SEND",
             }
         ])
