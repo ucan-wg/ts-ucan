2cb314e9973d74a193a871cc3af709ea89119531
Steven Vandevelde <icid.asset@gmail.com>
2022-04-05 15:50:35 +0200

    Improve NPM distribution

---
+++ scripts/package.js
@@ -0,0 +1,7 @@
+import fs from "fs"
+
+const CJS = { type: "commonjs" }
+const ESM = { type: "module" }
+
+fs.writeFileSync("./dist/cjs/package.json", JSON.stringify(CJS))
+fs.writeFileSync("./dist/esm/package.json", JSON.stringify(ESM))
