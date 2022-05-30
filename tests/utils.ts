d4f2468a69ce0e5037c47774714c5c6275ae5159
Philipp Krüger <philipp.krueger1@gmail.com>
2022-05-30 22:06:33 +0200

    :fire: Remove some redundant stuff

--- tests/utils.ts
+++
@@ -1,6 +0,0 @@
-export function maxNbf(parentNbf: number | undefined, childNbf: number | undefined): number | undefined {
-  if (parentNbf == null && childNbf == null) return undefined
-  if (parentNbf != null && childNbf != null) return Math.max(parentNbf, childNbf)
-  if (parentNbf != null) return parentNbf
-  return childNbf
-}
